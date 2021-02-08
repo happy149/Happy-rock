3787210420fc57e221c11358fb8ced3f2ff4b885
happy149 <hosnearahappybd@gmail.com>
2021-02-08 19:01:11 +0600

    before async

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const searchSongs = () => {
     const searchText = document.getElementById ('search-field').value;
-    const url = ` https://api.lyrics.ovh/suggest/${searchText}`
+    const url = ` https://api.lyrics.ovh/suggest/${searchText}`;
     fetch(url)
     .then(res=> res.json ())
     .then(data => displaySongs(data.data))
@@ -8,6 +8,8 @@ const searchSongs = () => {
 
 const displaySongs = songs => {
     const songContainer = document.getElementById ('song-container');
+    //ager gula fresh kora
+    songContainer.innerHTML = '';
     songs.forEach(song => {
         const songDiv = document.createElement('div');
         songDiv.className = 'single-result row align-items-center my-3 p-3';
@@ -20,7 +22,7 @@ const displaySongs = songs => {
            </audio>
         </div>
         <div class="col-md-3 text-md-right text-center">
-            <button onClick = "getLyric('${song.artist.name}','${song.preview}')" class="btn btn-success">Get Lyrics</button>
+            <button onClick = "getLyric('${song.artist.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
         </div>`;
         songContainer.appendChild(songDiv);
         
@@ -28,5 +30,59 @@ const displaySongs = songs => {
 }
 
 const getLyric = (artist,title) => {
-    console.log (artist,title)
+   const url = `https://api.lyrics.ovh/v1/${artist}/${title}`;
+   fetch(url)
+   .then(res => res.json())
+   .then(data => disPlayLyrics (data.lyrics))
+    
 }
+
+const disPlayLyrics = lyrics => {
+    const lyricsDiv = document.getElementById('song-lyrics')
+    lyricsDiv.innerText = lyrics;
+} 
+
+/*const searchSongs = () => {
+
+    const searchText = document.getElementById ('search-field').value;
+    const url = ` https://api.lyrics.ovh/suggest/${searchText}`;
+    fetch(url)
+    .then(res=> res.json ())
+    .then(data => displaySongs(data.data))
+}
+
+const displaySongs = songs => {
+    const songContainer = document.getElementById ('song-container');
+    //ager gula fresh kora
+    songContainer.innerHTML = '';
+    songs.forEach(song => {
+        const songDiv = document.createElement('div');
+        songDiv.className = 'single-result row align-items-center my-3 p-3';
+        songDiv.innerHTML = `
+        <div class="col-md-9">
+            <h3 class="lyrics-name">${song.title}</h3>
+            <p class="author lead">Album by <span>${song.artist.name}</span></p>
+            <audio controls>
+                 <source src="${song.preview}" type="audio/ogg">
+           </audio>
+        </div>
+        <div class="col-md-3 text-md-right text-center">
+            <button onClick = "getLyric('${song.artist.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
+        </div>`;
+        songContainer.appendChild(songDiv);
+        
+    })
+}
+
+const getLyric = (artist,title) => {
+   const url = `https://api.lyrics.ovh/v1/${artist}/${title}`;
+   fetch(url)
+   .then(res => res.json())
+   .then(data => disPlayLyrics (data.lyrics))
+    
+}
+
+const disPlayLyrics = lyrics => {
+    const lyricsDiv = document.getElementById('song-lyrics')
+    lyricsDiv.innerText = lyrics;
+} */
